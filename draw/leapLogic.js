32b91b878e5e43d2fddc410ff122ad92f5327cb0
Burr Sutter <burrsutter@gmail.com>
2013-09-19 21:46:14 -0400

    added refactored files

---
+++ draw/leapLogic.js
@@ -0,0 +1,39 @@
+// 
+// Credit for the drawing technique goes to
+// https://github.com/tmcw/leap-simple
+
+
+// Setup Leap loop with frame callback function
+function draw() {
+    var a, b;
+
+    for (var id in after) {
+        b = before[id],
+        a = after[id];
+        if (!b) continue;
+
+				var stroke = { 
+		  		'id' : id, 
+		  		'bx' : b.tipPosition.x,
+		  		'by' : b.tipPosition.y,
+		  		'ax' : a.tipPosition.x,
+		  		'ay' : a.tipPosition.y
+				};
+				strokeAsString = JSON.stringify(stroke);
+				publish("drawingAddress", strokeAsString);
+
+			  drawStroke(stroke);
+			  console.log(strokeAsString);
+    } // for
+
+    before = after;
+}
+
+Leap.loop(controllerOptions, function(frame, done) {
+    after = {};
+    for (var i = 0; i < frame.pointables.length; i++) {
+        after[frame.pointables[i].id] = frame.pointables[i];
+    }
+    draw();
+    done();
+});
